ed573ad6ea7ba0d5211d7e53fb68be01719211d4
MochiXu <mochix@moqi.ai>
2023-09-06 17:37:18 +0800

    update prepare_env.sh

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -7,4 +7,7 @@ tar -zxvf RQA.tar.gz
 cd RQA
 wget https://mqdb-release-1253802058.cos.ap-beijing.myqcloud.com/RQA/results-tgi.zip
 unzip results-tgi.zip
-python3 -m pip3 install -e .
+python3 -m pip install -e .
+
+# you can run scripts below:
+# python3 bench_model.py bench-rag -j "results-tgi/mmlu*-1.jsonl" -t 8 -i 10 -m llama-2-13B-ensemble-v5-a100
